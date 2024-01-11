da44f67a0a2444938bbf1cb50623cf9c74015d2b
nikec <nikec.job@gmail.com>
2024-01-11 23:58:25 +0100

    remove usage

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,14 +2,12 @@
 
 import { Demo } from '@/components/demo';
 import { Hero } from '@/components/hero';
-import { Usage } from '@/components/usage';
 
 export default function Page(): JSX.Element {
   return (
     <div className="flex flex-col items-center p-5 pt-16 sm:px-10 sm:pb-10">
       <Hero />
       <Demo />
-      <Usage />
     </div>
   );
 }
